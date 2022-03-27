2ab2a21fe706899b5e2aded8e0424ce535e81271
Daniel1945 <danielalarconcuesta@gmail.com>
2022-03-27 21:53:55 +0200

    User components finished

---
+++ src/components/UserBox/UserBox.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+
+import UserBoxProps from "../../interfaces/UserBoxProps";
+import "./UserBox.css";
+
+function UserBox(userBoxProps: UserBoxProps) {
+
+    const handleOnClick = () => {
+
+        if (userBoxProps.onClickHandler) {
+            userBoxProps.onClickHandler(userBoxProps.user);
+        }
+    }
+
+    const [containerClass, setContainerClass] = useState<string>("");
+
+    useEffect(() => {
+
+        let classes = "user-container";
+
+        if (userBoxProps.className) {
+            classes += ` ${userBoxProps.className}`;
+        }
+
+        setContainerClass(classes);
+
+
+    }, [userBoxProps.className])
+
+    return (
+        <div 
+            className={containerClass} 
+            onClick={handleOnClick}
+        >
+            <div className="user-name">
+                <span>
+                    {userBoxProps.user.name}
+                </span>
+            </div>
+
+            <div className="user-posts-number">
+                <div className="user-posts-number-circle">
+                    <span>
+                        {userBoxProps?.user?.posts ? userBoxProps.user.posts.length : 0}
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserBox;
---
+++ src/components/UserBoxList/UserBoxList.tsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+
+import User from "../../interfaces/User";
+import UserBox from "../UserBox/UserBox";
+import UserBoxListProps from "../../interfaces/UserBoxListProps";
+
+import { RootState } from "../../store";
+import { UserBoxListActionType } from "../../actions/userBoxListActions";
+import { useDispatch, useSelector  } from 'react-redux';
+
+import "./UserBoxList.css";
+
+function UserBoxList(userBoxListProps: UserBoxListProps) {
+
+    const dispatch = useDispatch();
+    const selectedUser: User | null = useSelector<RootState, User | null>((state) => state.userBoxList.selectedUser);
+    const nameFilter: string = useSelector<RootState, string>((state) => state.toolBar.nameFilter);
+
+    const [users, setUsers] = useState<User[]>([])
+
+    useEffect(() => {
+        
+        if (userBoxListProps.users) {
+            setUsers(filterByName(userBoxListProps.users.slice(0)));
+
+        } else {
+            setUsers([]);
+        }
+
+    }, [userBoxListProps.users, nameFilter])
+
+    useEffect(() => {
+
+        console.log("users changed", users);
+        
+        if (!users || !users.length) {
+            dispatch({
+                type: UserBoxListActionType.SELECT_USER,
+                payload: null
+            });
+
+        } else {
+            dispatch({
+                type: UserBoxListActionType.SELECT_USER,
+                payload: userBoxListProps.users[0]
+            });
+        }
+
+    }, [users])
+
+    const filterByName = ((users: User[]): User[] => {
+        
+        if (!users) {
+            return [];
+
+        } else if (!nameFilter) {
+            return users;
+        }
+
+        let filteredUsers:  User[] = users.filter((user: User) => {
+
+            if (user.name.includes(nameFilter)) return true;
+            else return false;
+        })
+
+        return filteredUsers;
+    })
+
+    const handleClickUser = (user: User) => {
+
+        if (!selectedUser || selectedUser.id != user.id) {
+            dispatch({
+                type: UserBoxListActionType.SELECT_USER,
+                payload: user
+            });
+        }
+    }
+
+    return (
+        <div className="user-list-container">
+
+            <div className="user-list-users">
+
+                {users.map((user: User) => (
+                    <UserBox
+                        key={user.id}
+                        className={selectedUser == user ? "user-container-active" : ""}
+                        onClickHandler={(user: User) =>  handleClickUser(user)}
+                        user={user}
+                    />
+                ))}
+
+            </div>
+        </div>
+    )
+}
+
+export default UserBoxList;
