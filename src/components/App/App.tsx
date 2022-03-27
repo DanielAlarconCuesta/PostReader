49839b591392a0f099d5376295646c5d4a24bd9a
Daniel1945 <danielalarconcuesta@gmail.com>
2022-03-27 21:56:46 +0200

    App component modified

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import logo from '../../logo.svg';
+import React, { useEffect } from "react";
+
+import Login from '../Login/Login';
+
+import Client from '../../interfaces/Client';
+
+import { useDispatch, useSelector  } from 'react-redux';
+import { RootState } from '../../store';
+
+import PostsView from '../PostsView/PostsView';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+
+	const dispatch = useDispatch();
+	const client: Client | null = useSelector<RootState, Client | null>((state) => state.session.client);
+
+	if (client) {
+		return (
+			<div className="app-posts-view">
+				<PostsView />
+			</div>
+		
+		)
+	
+	} else {
+		return(
+			<div className="app-login-container">
+				<Login />
+			</div>
+		)
+	}
 }
 
 export default App;
